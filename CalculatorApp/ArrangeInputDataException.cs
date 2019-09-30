661ad7edb06d7b9bafa1d05095f3d7fa1440eb1b
adugeen <konstantzts@gmail.com>
2019-09-30 11:53:49 +0500

    Добавлены все остальные тесты

--- CalculatorApp/ArrangeInputDataException.cs
+++ CalculatorApp/ArrangeInputDataException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CalculatorApp
 {
-    public class OutOfRangeException : Exception
+    class ArrangeInputDataException
     {
     }
 }
--- CalculatorLibTests/CalculatorTests.cs
+++ CalculatorLibTests/CalculatorTests.cs
@@ -31,24 +31,33 @@ namespace CalculatorLibTests
         [TestCategory("Function")]
         public void PressPlus()
         {
-            PressPlusNegativeItemAndZeroItem();
+            PressPlusTwoItems();
         }
 
         [TestMethod]
         [TestCategory("Function")]
-        public void PressMinus() { }
+        public void PressMinus()
+        {
+            PressMinusMultiplyPlusDivideFiveItems();
+        }
 
         [TestMethod]
         [TestCategory("Function")]
-        public void PressMultiply() { }
+        public void PressMultiply()
+        {
+            PressMultiplyTwoItems();
+        }
 
         [TestMethod]
         [TestCategory("Function")]
-        public void PressDivide() { }
+        public void PressDivide()
+        {
+            PressMinusMultiplyPlusDivideFiveItems();
+        }
 
         [TestMethod]
         [TestCategory("InputData")]
-        public void PressPlusNegativeItemAndZeroItem()
+        public void PressPlusTwoItems()
         {
             // Arrange.
             var value1 = -2.9;
@@ -67,6 +76,54 @@ namespace CalculatorLibTests
 
         [TestMethod]
         [TestCategory("InputData")]
+        public void PressMultiplyTwoItems()
+        {
+            // Arrange.
+            var value1 = 98.5;
+            var value2 = -9853;
+            // Act.
+            var calculator = new Calculator();
+            calculator.PressDisplay(value1);
+            calculator.PressMultiply();
+            calculator.PressDisplay(value2);
+            calculator.PressEnter();
+            var actual = calculator.Display;
+            // Assert.
+            var expected = -970520.5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("InputData")]
+        public void PressMinusMultiplyPlusDivideFiveItems()
+        {
+            // Arrange.
+            var value1 = 128;
+            var value2 = 6;
+            var value3 = 2;
+            var value4 = 22;
+            var value5 = 2;
+            // Act.
+            var calculator = new Calculator();
+            calculator.PressDisplay(value1);
+            calculator.PressMinus();
+            calculator.PressDisplay(value2);
+            calculator.PressMultiply();
+            calculator.PressDisplay(value3);
+            calculator.PressPlus();
+            calculator.PressDisplay(value4);
+            calculator.PressDivide();
+            calculator.PressDisplay(value5);
+            calculator.PressEnter();
+            calculator.PressEnter();
+            var actual = calculator.Display;
+            // Assert.
+            var expected = 133;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("InputData")]
         public void NoInputData()
         {
             // Arrange.
@@ -80,9 +137,42 @@ namespace CalculatorLibTests
         }
 
         [TestMethod]
+        [TestCategory("OutputData")]
+        public void OutPositiveValue()
+        {
+            PressMinusMultiplyPlusDivideFiveItems();
+        }
+
+        [TestMethod]
+        [TestCategory("OutputData")]
+        public void OutNegativeValue()
+        {
+            PressMultiplyTwoItems();
+        }
+
+        [TestMethod]
+        [TestCategory("OutputData")]
+        public void OutZero()
+        {
+            // Arrange.
+            var value1 = 1;
+            var value2 = 1;
+            // Act.
+            var calculator = new Calculator();
+            calculator.PressDisplay(value1);
+            calculator.PressMinus();
+            calculator.PressDisplay(value2);
+            calculator.PressEnter();
+            var actual = calculator.Display;
+            // Assert.
+            var expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         [TestCategory("RangePermissibleValue")]
         [ExpectedException(typeof(OutOfRangeException))]
-        public void PressPlusMinimumDoubleAndSomeItems()
+        public void PressPlusMinimumDoubleAndMinimumIntItem()
         {
             // Arrange. 
             var value1 = double.MinValue;
@@ -100,7 +190,7 @@ namespace CalculatorLibTests
         [TestMethod]
         [TestCategory("RangePermissibleValue")]
         [ExpectedException(typeof(OutOfRangeException))]
-        public void PressPlusMaximumDoubleAndSomeItems()
+        public void PressPlusMaximumDoubleAndMaximumShortItem()
         {
             // Arrange. 
             var value1 = double.MaxValue;
@@ -134,209 +224,32 @@ namespace CalculatorLibTests
         }
 
         [TestMethod]
-        [TestCategory("InputData")]
-        public void PressMultiplyTwoItem()
+        [TestCategory("LengthInputData")]
+        public void ZeroItems()
         {
-            // Arrange.
-            var value1 = 1;
-            var value2 = 2;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressMultiply();
-            calculator.PressDisplay(value2);
-            calculator.PressEnter();
+            NoInputData();
+        }
+
+        [TestMethod]
+        [TestCategory("LengthInputData")]
+        public void OneItem()
+        {
+            // Arrange. 
+            var value = 9;
+
+            // Act. 
+            Calculator calculator = new Calculator();
+            calculator.PressDisplay(value);
             var actual = calculator.Display;
-            // Assert.
-            var expected = 2;
-            Assert.AreEqual(expected, actual);
-        }
 
-        [TestMethod]
-        [TestCategory("InputData")]
-        public void PressDivideMultiplyTwoItem()
-        {
-            // Arrange.
-            var value1 = 1;
-            var value2 = 17;
-            var value3 = 34;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressDivide();
-            calculator.PressDisplay(value2);
-            calculator.PressMultiply();
-            calculator.PressDisplay(value3);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = 2;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [TestCategory("InputData")]
-        public void PressDivideTwoItem()
-        {
-            // Arrange.
-            var value1 = 1;
-            var value2 = 2;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressDivide();
-            calculator.PressDisplay(value2);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = 0.5;
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [TestMethod]
-        [TestCategory("InputData")]
-        public void PressPlusThreeItem()
-        {
-            // Arrange.
-            var value1 = 1;
-            var value2 = 2;
-            var value3 = 3;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressPlus();
-            calculator.PressDisplay(value2);
-            calculator.PressPlus();
-            calculator.PressDisplay(value3);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = 6;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [TestCategory("InputData")]
-        public void PressMinusTwoItem()
-        {
-            // Arrange.
-            var value1 = 3;
-            var value2 = 2;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressMinus();
-            calculator.PressDisplay(value2);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = 1;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [TestCategory("InputData")]
-        public void PressMinusThreeItem()
-        {
-            // Arrange.
-            var value1 = 3;
-            var value2 = 2;
-            var value3 = 2;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressMinus();
-            calculator.PressDisplay(value2);
-            calculator.PressMinus();
-            calculator.PressDisplay(value3);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = -1;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [TestCategory("InputData")]
-        public void PressPlusMinusThreeItem()
-        {
-            // Arrange.
-            var value1 = 3;
-            var value2 = 2;
-            var value3 = 2;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressPlus();
-            calculator.PressDisplay(value2);
-            calculator.PressMinus();
-            calculator.PressDisplay(value3);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = 3;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [TestCategory("InputData")]
-        public void PressMinusPlusThreeItem()
-        {
-            // Arrange.
-            var value1 = 3;
-            var value2 = 2;
-            var value3 = 1;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressMinus();
-            calculator.PressDisplay(value2);
-            calculator.PressPlus();
-            calculator.PressDisplay(value3);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = 2;
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [TestCategory("OutputData")]
-        public void OutPositiveValue()
-        {
-            PressMinusPlusThreeItem();
-        }
-
-        [TestMethod]
-        [TestCategory("OutputData")]
-        public void OutNegativeValue()
-        {
-            PressMinusThreeItem();
-        }
-
-        [TestMethod]
-        [TestCategory("OutputData")]
-        public void OutZero()
-        {
-            // Arrange.
-            var value1 = 1;
-            var value2 = 1;
-            // Act.
-            var calculator = new Calculator();
-            calculator.PressDisplay(value1);
-            calculator.PressMinus();
-            calculator.PressDisplay(value2);
-            calculator.PressEnter();
-            var actual = calculator.Display;
-            // Assert.
-            var expected = 0;
+            // Assert. 
+            var expected = 9;
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
         [TestCategory("LengthInputData")]
-        public void PresPlusTenItem()
+        public void PressPlusTenItem()
         {
             // Arrange.
             var array = new double[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -354,25 +267,102 @@ namespace CalculatorLibTests
         }
 
         [TestMethod]
-        [TestCategory("LengthInputData")]
-        public void PresPlusHundredItem()
+        [TestCategory("ArrangeInputData")]
+        [ExpectedException(typeof(ArrangeInputDataException))]
+        public void ItemByItem()
         {
             // Arrange.
+            var value1 = 8;
+            var value2 = 1.89;
+            var value3 = -23;
+            var value4 = 0;
 
-            // Act.
+            // Act. 
             var calculator = new Calculator();
-            Random rnd = new Random(42);
-            double expected = 0;
-            for (int i = 0; i < 100; i++)
-            {
-                var item = (double)rnd.Next(100);
-                expected += item;
-                calculator.PressDisplay(item);
-                calculator.PressPlus();
-            }
-            var actual = calculator.Display;
-            // Assert.
-            Assert.AreEqual(expected, actual);
+            calculator.PressDisplay(value1);
+            calculator.PressDisplay(value2);
+            calculator.PressDisplay(value3);
+            calculator.PressPlus();
+            calculator.PressDisplay(value4);
+            calculator.PressEnter();
+
+            // Assert. 
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("ArrangeInputData")]
+        [ExpectedException(typeof(ArrangeInputDataException))]
+        public void OperationByOperation()
+        {
+            // Arrange.
+            var value1 = 2;
+            var value2 = 98;
+            var value3 = 2;
+
+            // Act. 
+            var calculator = new Calculator();
+            calculator.PressDisplay(value1);
+            calculator.PressPlus();
+            calculator.PressMultiply();
+            calculator.PressDisplay(value2);
+            calculator.PressDivide();
+            calculator.PressDisplay(value3);
+            calculator.PressEnter();
+
+            // Assert. 
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("ArrangeInputData")]
+        [ExpectedException(typeof(ArrangeInputDataException))]
+        public void OperationFirst()
+        {
+            // Arrange.
+            var value1 = 12;
+            var value2 = 8.9;
+            var value3 = -1;
+
+            // Act. 
+            var calculator = new Calculator();
+            calculator.PressDivide();
+            calculator.PressDisplay(value1);
+            calculator.PressPlus();
+            calculator.PressDisplay(value2);
+            calculator.PressMinus();
+            calculator.PressDisplay(value3);
+            calculator.PressEnter();
+
+            // Assert. 
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("ArrangeInputData")]
+        [ExpectedException(typeof(ArrangeInputDataException))]
+        public void OperationLast()
+        {
+            // Arrange.
+            var value1 = 98;
+            var value2 = 21;
+            var value3 = 98;
+            var value4 = 2;
+
+            // Act. 
+            var calculator = new Calculator();
+            calculator.PressDisplay(value1);
+            calculator.PressPlus();
+            calculator.PressDisplay(value2);
+            calculator.PressMinus();
+            calculator.PressDisplay(value3);
+            calculator.PressDivide();
+            calculator.PressDisplay(value4);
+            calculator.PressMultiply();
+            calculator.PressEnter();
+
+            // Assert. 
+            Assert.Fail();
         }
     }
 }
